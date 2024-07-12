38f315547f0a61f2ad8498397a58be9e852fc62d
Davi Machado da Rocha <davimrocha@gmail.com>
2024-07-12 22:26:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def display_map(view_type):
     return iframe
 
 description = """
-<div style="text-align: justify;">
+<div style="text-align: justify; margin-bottom: 20px;">
     <h1>Projeto de Mapeamento das Obras de Machado de Assis</h1>
     <p>Este estudo tem o objetivo de desenvolver uma aplicação web semântica que mapeia localidades geográficas nas obras de Machado de Assis, armazenando-as em uma triplestore. A partir da integração dos dados disponibilizados pela enciclopédia MachadodeAssis.net com as coordenadas geográficas de Geonames.org e GoogleMaps, o projeto visa oferecer uma experiência de leitura através de mapas interativos, que servirão de suporte para as menções aos espaços realizadas pelo escritor ao longo do Século XIX.</p>
     <p>Para a extração das citações, a aplicação utiliza a biblioteca python BeautifulSoup que realiza consultas, requisições e coleta os dados da enciclopédia estruturando-os de acordo com os parâmetros do schema.org. As citações coletadas serão submetidas aos modelos gpt3.5-instruct e gpt4-turbo com o intuito de obter os nomes atuais das localidades, bem como a devida classificação destes espaços de acordo com a ontologia Geonames.org.</p>
@@ -35,12 +35,11 @@ view_type = gr.Radio(
     label="Selecione a visão do mapa"
 )
 
-iface = gr.Interface(
-    fn=display_map,
-    inputs=view_type,
-    outputs="html",
-    live=True,
-    description=description,
-    layout="vertical"
-)
-iface.launch()
+with gr.Blocks() as demo:
+    gr.Markdown(description)
+    with gr.Column():
+        view_type.render()
+        map_display = gr.HTML()
+    view_type.change(fn=display_map, inputs=view_type, outputs=map_display)
+
+demo.launch()
