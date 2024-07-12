29b0c28cc76603733ed84a337530e937ca4f6dcc
Davi Machado da Rocha <davimrocha@gmail.com>
2024-07-12 22:25:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,19 +18,29 @@ def display_map(view_type):
     return iframe
 
 description = """
-Este estudo tem o objetivo de desenvolver uma aplicação web semântica que mapeia localidades geográficas nas obras de Machado de Assis, armazenando-as em uma triplestore. A partir da integração dos dados disponibilizados pela enciclopédia MachadodeAssis.net com as coordenadas geográficas de Geonames.org e GoogleMaps, o projeto visa oferecer uma experiência de leitura através de mapas interativos, que servirão de suporte para as menções aos espaços realizadas pelo escritor ao longo do Século XIX. Para a extração das citações, a aplicação utiliza a biblioteca python BeautifulSoup que realiza consultas, requisições e coleta os dados da enciclopédia estruturando-os de acordo com os parâmetros do schema.org. As citações coletadas serão submetidas aos modelos gpt3.5-instruct e gpt4-turbo com o intuito de obter os nomes atuais das localidades, bem como a devida classificação destes espaços de acordo com a ontologia Geonames.org. Ao final, são realizadas consultas SPARQL ao portal dados.literaturabrasileira.ufsc.br com o objetivo de obter identificadores únicos para cada livro, oferecendo uma integração entre mapas, citações e textos completos, em consonância com os padrões Linked Data.
+<div style="text-align: justify;">
+    <h1>Projeto de Mapeamento das Obras de Machado de Assis</h1>
+    <p>Este estudo tem o objetivo de desenvolver uma aplicação web semântica que mapeia localidades geográficas nas obras de Machado de Assis, armazenando-as em uma triplestore. A partir da integração dos dados disponibilizados pela enciclopédia MachadodeAssis.net com as coordenadas geográficas de Geonames.org e GoogleMaps, o projeto visa oferecer uma experiência de leitura através de mapas interativos, que servirão de suporte para as menções aos espaços realizadas pelo escritor ao longo do Século XIX.</p>
+    <p>Para a extração das citações, a aplicação utiliza a biblioteca python BeautifulSoup que realiza consultas, requisições e coleta os dados da enciclopédia estruturando-os de acordo com os parâmetros do schema.org. As citações coletadas serão submetidas aos modelos gpt3.5-instruct e gpt4-turbo com o intuito de obter os nomes atuais das localidades, bem como a devida classificação destes espaços de acordo com a ontologia Geonames.org.</p>
+    <p>Ao final, são realizadas consultas SPARQL ao portal dados.literaturabrasileira.ufsc.br com o objetivo de obter identificadores únicos para cada livro, oferecendo uma integração entre mapas, citações e textos completos, em consonância com os padrões Linked Data.</p>
+</div>
 """
 
-view_type = gr.Radio(["Mapa de locais citados no conjunto da obra",
-                      "Mapa de calor com a frequência de locais citados no conjunto da obra",
-                      "Visão de local e citação sobre Copacabana com Google StreetView",
-                      "Mapa de citações por obra",
-                      "Mapa de calor de citações por obra"], 
-                     label="Selecione a visão do mapa")
+view_type = gr.Radio(
+    ["Mapa de locais citados no conjunto da obra",
+     "Mapa de calor com a frequência de locais citados no conjunto da obra",
+     "Visão de local e citação sobre Copacabana com Google StreetView",
+     "Mapa de citações por obra",
+     "Mapa de calor de citações por obra"],
+    label="Selecione a visão do mapa"
+)
 
-iface = gr.Interface(fn=display_map, 
-                     inputs=view_type, 
-                     outputs="html", 
-                     live=True, 
-                     description=description)
+iface = gr.Interface(
+    fn=display_map,
+    inputs=view_type,
+    outputs="html",
+    live=True,
+    description=description,
+    layout="vertical"
+)
 iface.launch()
