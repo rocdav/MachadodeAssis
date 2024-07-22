9be5705861b74468ca766f39526d39673d1171a2
Davi Machado da Rocha <davimrocha@gmail.com>
2024-07-22 19:38:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ description = """
     <p>Este estudo tem o objetivo de desenvolver uma aplicação web semântica que mapeia localidades geográficas nas obras de Machado de Assis, armazenando-as em uma triplestore. A partir da integração dos dados disponibilizados pela enciclopédia MachadodeAssis.net com as coordenadas geográficas de Geonames.org e GoogleMaps, o projeto visa oferecer uma experiência de leitura através de mapas interativos, que servirão de suporte para as menções aos espaços realizadas pelo escritor ao longo do Século XIX.</p>
     <p>Para a extração das citações, a aplicação utiliza a biblioteca python BeautifulSoup que realiza consultas, requisições e coleta os dados da enciclopédia estruturando-os de acordo com os parâmetros do schema.org. As citações coletadas serão submetidas aos modelos gpt3.5-instruct e gpt4-turbo com o intuito de obter os nomes atuais das localidades, bem como a devida classificação destes espaços de acordo com a ontologia Geonames.org.</p>
     <p>Ao final, são realizadas consultas SPARQL ao portal dados.literaturabrasileira.ufsc.br com o objetivo de obter identificadores únicos para cada livro, oferecendo uma integração entre mapas, citações e textos completos, em consonância com os padrões Linked Data.</p>
+    <p>A imagem abaixo ilustra a estrutura de dados utilizada no projeto:</p>
+    <img src="https://huggingface.co/spaces/histlearn/MachadodeAssis/blob/main/grafooo.png" alt="Estrutura de Dados" style="width:100%; height:auto;">
 </div>
 """
 
