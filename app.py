d8a53f8325a32d29163a5b7aa917ba147c4abc42
Davi Machado da Rocha <davimrocha@gmail.com>
2024-07-22 19:42:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ def display_map(view_type):
     iframe = f'<iframe src="{url}" width="100%" height="600" frameborder="0"></iframe>'
     return iframe
 
+def display_sparql():
+    url = "https://huggingface.co/spaces/histlearn/JenaFuseki"
+    iframe = f'<iframe src="{url}" width="100%" height="600" frameborder="0"></iframe>'
+    return iframe
+
 description = """
 <div style="text-align: justify; margin-bottom: 20px;">
     <h1>Projeto de Mapeamento das Obras de Machado de Assis</h1>
@@ -24,7 +29,7 @@ description = """
     <p>Para a extração das citações, a aplicação utiliza a biblioteca python BeautifulSoup que realiza consultas, requisições e coleta os dados da enciclopédia estruturando-os de acordo com os parâmetros do schema.org. As citações coletadas serão submetidas aos modelos gpt3.5-instruct e gpt4-turbo com o intuito de obter os nomes atuais das localidades, bem como a devida classificação destes espaços de acordo com a ontologia Geonames.org.</p>
     <p>Ao final, são realizadas consultas SPARQL ao portal dados.literaturabrasileira.ufsc.br com o objetivo de obter identificadores únicos para cada livro, oferecendo uma integração entre mapas, citações e textos completos, em consonância com os padrões Linked Data.</p>
     <p>A imagem abaixo ilustra a estrutura de dados utilizada no projeto:</p>
-    <img src="grafooo.png" alt="Estrutura de Dados" style="width:100%; height:auto;">
+    <img src="https://huggingface.co/spaces/histlearn/MachadodeAssis/blob/main/grafooo.png" alt="Estrutura de Dados" style="width:100%; height:auto;">
 </div>
 """
 
@@ -43,5 +48,6 @@ with gr.Blocks() as demo:
         view_type.render()
         map_display = gr.HTML()
     view_type.change(fn=display_map, inputs=view_type, outputs=map_display)
+    gr.Button("Ver endpoint SPARQL").click(fn=display_sparql, outputs=map_display)
 
 demo.launch()
